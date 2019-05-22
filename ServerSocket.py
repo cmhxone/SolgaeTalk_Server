070b9250fa7e5cb0a7ecb23eec7643fb6d9e1e8a
cmhxone <cmhxone@outlook.kr>
2019-05-22 20:38:50 +0900

    로그저장 수정

--- ServerSocket.py
+++ ServerSocket.py
@@ -25,25 +25,25 @@ class ServerSocket:
 		# 소켓 생성
 		try:
 			self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	# 서버 소켓을 생성한다
-			#print("소켓 생성 완료")
+			print("소켓 생성 완료")
 		except socket.error as er:
-			#print("소켓 생성 실패")
+			print("소켓 생성 실패")
 			exit(-1)
 
 		# 소켓 연동
 		try:
 			self.__socket.bind((host, port))	# 소켓을 포트, 호스트와 연결한다
-			#print("소켓 연동 성공")
+			print("소켓 연동 성공")
 		except socket.error as er:
-			#print("소켓 연동 실패")
+			print("소켓 연동 실패")
 			exit(-1)
 
 		# 소켓 연결 대기
 		try:
 			self.__socket.listen(5)
-			#print("소켓 연결대기 성공")
+			print("소켓 연결대기 성공")
 		except socket.error as er:
-			#print("소켓 연결대기 실패")
+			print("소켓 연결대기 실패")
 			exit(-1)
 
 	# 클라이언트로부터 메시지를 읽어오는 함수
@@ -74,11 +74,11 @@ class ServerSocket:
 							able = True
 					except Exception as e:
 						able = False
-						#print(e)
+						print(e)
 
 				except:
 					self.DestroyClient(clientSocket)
-					#print(addr, "과의 접속이 종료되었습니다")
+					print(addr, "과의 접속이 종료되었습니다")
 					break
 
 			except ConnectionResetError:	# 클라이언트가 비정상적으로 종료된 경우
@@ -86,17 +86,17 @@ class ServerSocket:
 				db = pymysql.connect(host=self.__host, port=3306, user="Solgae", passwd="gntech2152", db="SolgaeTalk", charset="utf8", autocommit=True)
 				cursor = db.cursor()
 				cursor.execute("UPDATE Accounts SET online=False WHERE nickname='" + nickname +"'")
-				#print(nickname, addr, "과의 접속이 종료되었습니다")
+				print(nickname, addr, "과의 접속이 종료되었습니다")
 				break
 
 			# 메시지 수신 플래그는 5002, 이 플래그를 전송 받은경우 모든 클라이언트에 메시지를 출력해준다
 			if message[0] == 5002:
-				#print("[", message[1].decode().replace("\00", "") , "] ", message[2].decode() )
+				print("[", message[1].decode().replace("\00", "") , "] ", message[2].decode() )
 				self.SendMessage(data)
 
 			# 접속 플래그는 1996, 이 플래그를 전송 받은 경우 모든 클라이언트에 접속 메시지를 출력해준다
 			elif message[0] == 1996:
-				#print(nickname, addr, "에서 정식 클라이언트를 통해 접속하였습니다")
+				print(nickname, addr, "에서 정식 클라이언트를 통해 접속하였습니다")
 				self.__lock.acquire
 				self.__SocketList.append(clientSocket)
 				self.SendMessage(data)
@@ -104,7 +104,7 @@ class ServerSocket:
 
 			# 접속 종료 플래그는 2015, 이 플래그를 전송 받은 경우 모든 클라이언트에 접속 종료 메시지를 출력해준다
 			elif message[0] == 2015:
-				#print(nickname, addr, "이 접속종료를 요청하였습니다")
+				print(nickname, addr, "이 접속종료를 요청하였습니다")
 				self.SendMessage(data)
 				self.DestroyClient(clientSocket)
 				db = pymysql.connect(host=self.__host, port=3306, user="Solgae", passwd="gntech2152", db="SolgaeTalk", charset="utf8", autocommit=True)
@@ -115,7 +115,7 @@ class ServerSocket:
 			# 이외의 검증되지 않은 플래그들은 접속을 종료한다
 			else:
 				self.DestroyClient(clientSocket)
-				#print(addr, "과의 접속이 종료되었습니다")
+				print(addr, "과의 접속이 종료되었습니다")
 				break
 	
 	# 클라이언트 소켓을 할당 해제하는 함수
@@ -135,7 +135,7 @@ class ServerSocket:
 			for socket in self.__SocketList:
 				socket.send(data)
 		except:
-			#print("Sending Failure")
+			print("Sending Failure")
 			pass
 		self.__lock.release
 
@@ -152,14 +152,14 @@ class ServerSocket:
 				db = pymysql.connect(host=self.__host, port=3306, user="Solgae", passwd="gntech2152", db="SolgaeTalk", charset="utf8", autocommit=True)
 				cursor = db.cursor()
 				cursor.execute("UPDATE Accounts SET online=False")
-				#print("서버가 종료되었습니다")
+				print("서버가 종료되었습니다")
 				exit(0)
 			
 			# 소켓의 접속여부를 확인한다
 			if clientSocket in self.__SocketList:
 				pass
 			else:
-				#print(addr, "에서 접속하였습니다")
+				print(addr, "에서 접속하였습니다")
 				# 접속이 완료된 경우 별도의 스레드를 생성하여 메시지 처리를 관할한다
 				t = threading.Thread(target=self.ProcessMessage, args=(clientSocket, addr))
 				t.daemon = True
