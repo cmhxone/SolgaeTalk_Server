cd44750ffc695cb59dd01a289bb48024864fdd15
Cho MuHyeon <47947882+cmhxone@users.noreply.github.com>
2020-11-23 10:00:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import ServerSocket
 import threading
 
 # 메인 실행부분
-server = ServerSocket.ServerSocket(7346, "muhyeon.com", 1024)
+#server = ServerSocket.ServerSocket(#Port, #Host, 1024)
 serverThread = threading.Thread(target=server.Start(), args=())
 serverThread.daemon = True
 serverThread.start()
